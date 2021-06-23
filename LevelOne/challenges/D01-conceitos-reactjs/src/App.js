6f6b69c918e93dab48d671f607f8afc80267a1e5
Carlos <carloshalbuquerque2@gmail.com>
2021-06-23 10:41:32 -0300

    React Challenge

--- LevelOne/challenges/D01-conceitos-reactjs/src/App.js
+++ LevelOne/challenges/D01-conceitos-reactjs/src/App.js
@@ -1,29 +1,71 @@
-import React from "react";
-
-import "./styles.css";
+import React, { useState, useEffect } from "react";
+import api from "./services/api";
+import "./style.css";
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get("repositories/").then((response) => {
+      setRepositories(response.data);
+    });
+  }, []);
+
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post("repositories", {
+      title: `Repository ${Date.now()}`,
+      url: "www.google.com.br",
+      techs: "React, JS",
+    });
+
+    const repository = response.data;
+
+    setRepositories([...repositories, repository]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    await api.delete(`repositories/${id}`);
+
+    const newRepositories = repositories.filter(
+      repository => repository.id != id
+    )
+
+    setRepositories(newRepositories)
   }
 
   return (
     <div>
+
+      <div class= "title">
+        <h1>Desafio React - Repository</h1>
+      </div>
+
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        <button onClick={handleAddRepository}>
+          Adicionar novo repositório
+        </button>
       </ul>
+        {repositories.map(
+          (repository) => (
+            <div>
+              <section class="card">
+                <div>
+                  <header>
+                    <h2 key={repository.id}> {repository.title}</h2>
+                  </header>
+                  <p> URL: {repository.url}</p>
+                  <p> Techs: {repository.techs}</p>
+                  <p> Likes: {repository.likes}</p>
+                </div>
+              </section>
 
-      <button onClick={handleAddRepository}>Adicionar</button>
+              <button onClick={() => handleRemoveRepository(repository.id)}>
+                Remover o repositório: {repository.title}
+              </button>
+
+            </div>
+          )
+        )}
     </div>
   );
 }
