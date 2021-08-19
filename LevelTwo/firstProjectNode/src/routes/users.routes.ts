b4f78d1fb48cd6e3eafdb06e6a49ee1010d72b75
Carlos <carloshalbuquerque2@gmail.com>
2021-08-19 17:48:16 -0300

    att typescript

--- LevelTwo/firstProjectNode/src/routes/users.routes.ts
+++ LevelTwo/firstProjectNode/src/routes/users.routes.ts
@@ -20,9 +20,16 @@ usersRouter.post('/', async (request, response) => {
       password,
     });
 
-    delete user.password;
+    // Com a atualização do TypeScript, isso se faz necessário
+    const userWithoutPassword = {
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      created_at: user.created_at,
+      updated_at: user.updated_at,
+    };
 
-    return response.json(user);
+    return response.json(userWithoutPassword);
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
