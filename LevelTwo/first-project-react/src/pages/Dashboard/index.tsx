098a7ed374f54890fefd34fb0ef943dc01154895
Carlos <carloshalbuquerque2@gmail.com>
2021-09-15 16:36:23 -0300

    add style on Dashboard

--- LevelTwo/first-project-react/src/pages/Dashboard/index.tsx
+++ LevelTwo/first-project-react/src/pages/Dashboard/index.tsx
@@ -1,7 +1,51 @@
 // eslint-disable-next-line no-use-before-define
 import React from 'react';
-import { Title } from './styles';
+import { FiChevronRight } from 'react-icons/fi';
 
-const Dashboard: React.FC = () => <Title>Explore repositórios no GitHub</Title>;
+import logoImg from '../../assets/githubExplorer.svg';
+
+import { Title, Form, Repositories } from './styles';
+
+const Dashboard: React.FC = () => (
+  <>
+    <img src={logoImg} alt="" />
+    <Title>Explore repositórios no GitHub</Title>
+
+    <Form>
+      <input placeholder="Digite o nome do repositório" />
+      <button type="submit">Pesquisar</button>
+    </Form>
+
+    <Repositories>
+      <a href="https://github.com/">
+        <img src="https://avatars.githubusercontent.com/u/55409050?v=4" alt="Carlos" />
+        <div>
+          <strong>React Carlos</strong>
+          <p>Easy peasy React JS</p>
+        </div>
+        <FiChevronRight size={20} />
+      </a>
+
+      <a href="https://github.com/">
+        <img src="https://avatars.githubusercontent.com/u/55409050?v=4" alt="Carlos" />
+        <div>
+          <strong>React Carlos</strong>
+          <p>Easy peasy React JS</p>
+        </div>
+        <FiChevronRight size={20} />
+      </a>
+
+      <a href="https://github.com/">
+        <img src="https://avatars.githubusercontent.com/u/55409050?v=4" alt="Carlos" />
+        <div>
+          <strong>React Carlos</strong>
+          <p>Easy peasy React JS</p>
+        </div>
+        <FiChevronRight size={20} />
+      </a>
+
+    </Repositories>
+  </>
+);
 
 export default Dashboard;
