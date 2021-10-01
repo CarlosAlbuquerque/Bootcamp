15ce03afa0a9018d2d174da8c0b5a6b761630d17
Carlos <carloshalbuquerque2@gmail.com>
2021-10-01 09:36:59 -0300

    Add Global Style

--- LevelTwo/go-barber/src/App.tsx
+++ LevelTwo/go-barber/src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import GlobalStyle from './styles/global';
 
-const App: React.FC = () => {
-  return (
+const App: React.FC = () => (
+  <>
     <h1>Go Barber</h1>
-  )
-}
+    <GlobalStyle />
+  </>
+);
 
 export default App;
