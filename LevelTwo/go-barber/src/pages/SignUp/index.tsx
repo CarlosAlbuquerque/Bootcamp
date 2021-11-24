929535b0cfdfc6840047c2e2ba73f7f3b44c8c41
Carlos <carloshalbuquerque2@gmail.com>
2021-11-24 17:25:04 -0300

    add new pages

---
+++ LevelTwo/go-barber/src/pages/SignUp/index.tsx
@@ -0,0 +1,48 @@
+/* eslint-disable react/jsx-no-bind */
+import React from 'react';
+import {
+  FiMail, FiLock, FiUser, FiArrowLeft,
+} from 'react-icons/fi';
+import { Form } from '@unform/web';
+import logoImg from '../../assets/logo.svg';
+
+import Input from '../../components/Input/index';
+import Button from '../../components/Button/index';
+
+import { Container, Content, Background } from './styles';
+
+const SignUp: React.FC = () => {
+  function handleSubmit(data: any): void {
+    console.log(data);
+  }
+
+  return (
+    <Container>
+
+      <Background />
+
+      <Content>
+        <img src={logoImg} alt="GoBarber" />
+
+        <Form onSubmit={handleSubmit}>
+          <h1>Faça seu cadastro</h1>
+
+          <Input name="name" icon={FiUser} placeholder="Nome" />
+          <Input name="email" icon={FiMail} placeholder="E-mail" />
+          <Input name="password" icon={FiLock} type="password" placeholder="Password" />
+
+          <Button type="submit">Cadastrar</Button>
+
+        </Form>
+
+        <a href="create-account">
+          <FiArrowLeft />
+          Voltar para logon
+        </a>
+      </Content>
+
+    </Container>
+  );
+};
+
+export default SignUp;
